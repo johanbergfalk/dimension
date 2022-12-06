e2280494638ac803a4a41a4e141e7f743b38d387
sebastiansela <112576272+sebastiansela@users.noreply.github.com>
2022-12-06 13:58:17 +0100

    Add files via upload
    Bitmap Decoder class

---
+++ dimension/app/app/src/main/java/com/example/dimension/Model/BitmapDecode.java
@@ -0,0 +1,22 @@
+package com.example.dimension.Model;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+public class BitmapDecode {
+
+    public static Bitmap stringToBitmap(String string) {
+
+        Bitmap bitmap = null;
+        try {
+            byte[] bitmapArray;
+            bitmapArray = Base64.decode(string, Base64.DEFAULT);
+            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0,
+                    bitmapArray.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+}
